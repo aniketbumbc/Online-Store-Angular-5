3f9cb404c73ac28b823c5928726f436f5c50d9e9
aniket umbc <aniket.umbc@gmail.com>
2018-01-10 09:00:11 +0530

    add row component for image, price, breadcrumbs

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -15,9 +15,23 @@ import {Product} from '../product.model';
 export class ProductsListComponent implements OnInit {
 
   @Input()productList:Product[];   // gets input expected to receive from product model which is list of products
-  @Output() onProductSelected:EventEmitter<Product>; // send data componet to outside world 
-  constructor() { }
+  @Output() onProductSelected:EventEmitter<Product>; // send data componet to outside world.
 
+private currentProduct=Product;
+constructor() {
+  this.onProductSelected=new EventEmitter();
+ }
+
+clicked(product:Product):void{
+  this.currentProduct=Product;
+  this.onProductSelected.emit(product);
+}
+isSelected(product:Product):boolean{
+if(!product||this.currentProduct){
+  return false;
+}
+return product.sku===this.currentProduct.sku;
+}
   ngOnInit() {
   }
 
